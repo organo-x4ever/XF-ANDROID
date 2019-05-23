69c65ff7127737c39221206b7e9edadf9d445c3e
Gurpreet Deol <deol.gs@hotmail.com>
2019-05-22 18:28:33 -0700

    Update for News from HTML page dynamically in App_Dynamic directory in API

--- X4Ever.Android/xchallenge/com.organo.xchallenge/Pages/Blog/BlogPage.xaml.cs
+++ X4Ever.Android/xchallenge/com.organo.xchallenge/Pages/Blog/BlogPage.xaml.cs
@@ -6,6 +6,7 @@ using com.organo.xchallenge.Handler;
 using com.organo.xchallenge.Services;
 using Xamarin.Forms;
 using com.organo.xchallenge.Controls;
+using com.organo.xchallenge.Statics;
 
 namespace com.organo.xchallenge.Pages.Blog
 {
@@ -18,10 +19,7 @@ namespace com.organo.xchallenge.Pages.Blog
             try
             {
                 InitializeComponent();
-                _model = new BlogViewModel()
-                {
-                    Root = root
-                };
+                _model = new BlogViewModel() { Root = root };
                 Init();
             }
             catch (Exception ex)
@@ -35,16 +33,18 @@ namespace com.organo.xchallenge.Pages.Blog
             await App.Configuration.InitialAsync(this);
             NavigationPage.SetHasNavigationBar(this, false);
             BindingContext = _model;
+            _model.WebUri = await _model.GetLink();
+            var webUri =
+                $"?token={App.Configuration.UserToken}&theme=dark&languagecode={App.Configuration?.AppConfig.DefaultLanguage}&active=true&application={App.Configuration.GetApplication()}";
 
-            HybridWebView hybridWebView = new HybridWebView()
+            contentView.Content = new HybridWebView()
             {
                 HorizontalOptions = LayoutOptions.FillAndExpand,
                 VerticalOptions = LayoutOptions.FillAndExpand,
-                Margin = new Thickness(0, -6, 0, 0)
+                Margin = new Thickness(0, -6, 0, 0),
+                BackgroundColor = Palette._MainBackground,
+                Uri = _model.WebUri + webUri
             };
-            _model.WebUri = await _model.GetLink();
-            hybridWebView.Uri = _model.WebUri;
-            contentView.Content = hybridWebView;
         }
 
         protected override bool OnBackButtonPressed()
